a6fdbf9fdf997f112330cccad8949f356146d6e9
Your Name <you@example.com>
2021-06-21 20:39:15 -0500

    Tweaked studio more

--- src/main/java/org/launchcode/codingevents/models/EventType.java
+++ src/main/java/org/launchcode/codingevents/models/EventType.java
@@ -1,32 +1,18 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Size;
+public enum EventType {
+    CONFERENCE("Conference"),
+    MEETUP("Meetup"),
+    WORKSHOP("Workshop"),
+    SOCIAL("Social");
 
-public class EventType {
+    private final String displayName;
 
-    private String name;
-
-    public EventType(@Size(min = 3, message = "Name must be at least 3 characters long.") String name) {
-        this.name = name;
+    EventType(String displayName) {
+        this.displayName = displayName;
     }
 
-    public EventType(){ }
-
-    private EventType type;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public EventType getType() {
-        return type;
-    }
-
-    public void setType(EventType type) {
-        this.type = type;
+    public String getDisplayName() {
+        return displayName;
     }
 }
